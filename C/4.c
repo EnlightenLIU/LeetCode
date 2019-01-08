2b245e02cf1b2e12df66acfae461d8dfba68fdc5
June95 <17634423669@163.com>
2019-01-08 09:09:42 +0800

    删除多余及新加

--- C/4.c
+++ C/4.c
@@ -3,27 +3,65 @@
 double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
     int *nums;
-    if(nums1Size==0)
+    int m=nums1Size;
+    int n=nums2Size;
+    if (m == 0)
     {
-        if(nums2Size%2==0)
+        if (n % 2 == 0)
         {
-            return (nums2[nums2Size/2-1]+nums2[nums2Size/2])/2;
+            return (nums2[n / 2 - 1] + nums2[n / 2]) / 2;
         }
         else
         {
-            return nums2[nums2Size/2];
+            return nums2[n / 2];
         }
     }
-    else if(nums2Size==0)
+    else if (n == 0)
     {
-        if (nums1Size % 2 == 0)
+        if (m % 2 == 0)
         {
-            return (nums1[nums1Size / 2 - 1] + nums1[nums1Size / 2]) / 2;
+            return (nums1[m / 2 - 1] + nums1[m / 2]) / 2;
         }
         else
         {
-            return nums1[nums1Size / 2];
+            return nums1[m / 2];
         }
     }
-    
+    int count = 0;
+    int i = 0, j = 0;
+    while (count != (m * n))
+    {
+        if (i == m)
+        {
+            while (j != n)
+            {
+                nums[count++] = nums2[i++];
+            }
+            break;
+        }
+        if (j == n)
+        {
+            while (i != m)
+            {
+                nums[count++] = nums1[i++];
+            }
+            break;
+        }
+        if (nums1[i] < nums2[j])
+        {
+            nums[count++] = nums1[i++];
+        }
+        else
+        {
+            nums[count++] = nums2[i++];
+        }
+    }
+    if (count % 2 == 0)
+    {
+        return (nums1[count / 2 - 1] + nums1[count / 2]) / 2;
+    }
+    else
+    {
+        return nums1[count / 2];
+    }
 }
