2ca1ae4ee16287d3cb4baa04f40eb5ac12dc1ec9
June95 <17634423669@163.com>
2019-01-09 11:00:38 +0800

    增加C例题

--- C/4.c
+++ C/4.c
@@ -2,66 +2,17 @@
 #include <stdio.h>
 double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
-    int *nums;
-    int m=nums1Size;
-    int n=nums2Size;
-    if (m == 0)
-    {
-        if (n % 2 == 0)
-        {
-            return (nums2[n / 2 - 1] + nums2[n / 2]) / 2;
-        }
-        else
-        {
-            return nums2[n / 2];
-        }
-    }
-    else if (n == 0)
-    {
-        if (m % 2 == 0)
-        {
-            return (nums1[m / 2 - 1] + nums1[m / 2]) / 2;
-        }
-        else
-        {
-            return nums1[m / 2];
-        }
-    }
-    int count = 0;
-    int i = 0, j = 0;
-    while (count != (m * n))
-    {
-        if (i == m)
-        {
-            while (j != n)
-            {
-                nums[count++] = nums2[i++];
-            }
-            break;
-        }
-        if (j == n)
-        {
-            while (i != m)
-            {
-                nums[count++] = nums1[i++];
-            }
-            break;
-        }
-        if (nums1[i] < nums2[j])
-        {
-            nums[count++] = nums1[i++];
-        }
-        else
-        {
-            nums[count++] = nums2[i++];
-        }
-    }
-    if (count % 2 == 0)
-    {
-        return (nums1[count / 2 - 1] + nums1[count / 2]) / 2;
-    }
-    else
-    {
-        return nums1[count / 2];
-    }
+    //int *nums;
+
+    int m = nums1Size;
+    int n = nums2Size;
+}
+
+void main()
+{
+    int a[4] = {1, 3, 5, 7};
+    int b[4] = {2, 4, 6, 8};
+    double c;
+    c = findMedianSortedArrays(a, 4, b, 4);
+    printf("%f", c);
 }
