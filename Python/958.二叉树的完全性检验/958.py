a1dbe865d5f5bd45c3fcee8aee3e5366e9d75429
June95 <853695334@qq.com>
2019-04-15 21:32:34 +0800

    958

--- Python/958.二叉树的完全性检验/958.py
+++ Python/958.二叉树的完全性检验/958.py
@@ -2,7 +2,7 @@
 @Author: 拎把木剑当大侠
 @Description: 
 @Date: 2019-04-15 21:08:16
-@LastEditTime: 2019-04-15 21:08:27
+@LastEditTime: 2019-04-15 21:32:15
 '''
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -12,7 +12,7 @@
 #         self.right = None
 
 
-class Solution(object):
+class Solution:
     def isCompleteTree(self, root):
         """
         :type root: TreeNode
@@ -24,17 +24,19 @@ class Solution(object):
         flag = 0
         while q:
             qq = []
-            for nd in q:
+            for node in q:
                 if flag:
-                    if nd.left or nd.right:
+                    if node.left or node.right:
                         return False
-                if nd.left and nd.right:
-                    qq.append(nd.left)
-                    qq.append(nd.right)
-                elif nd.left:
-                    qq.append(nd.left)
+                    if node.left and node.right:
+                        return False
+                if node.left and node.right:
+                    qq.append(node.left)
+                    qq.append(node.right)
+                elif node.left:
+                    qq.append(node.left)
                     flag = 1
-                elif nd.right:
+                elif node.right:
                     return False
                 else:
                     flag = 1
